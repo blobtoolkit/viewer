9671a7fc702fe90c2980173206e60009daa83734
Richard Challis <rjchallis@gmail.com>
2019-09-18 10:48:55 +0100

    improve filter preview bar styling

--- src/client/views/components/DatasetApplyFilters.jsx
+++ src/client/views/components/DatasetApplyFilters.jsx
@@ -21,7 +21,7 @@ const mapDispatchToProps = dispatch => {
 class ApplyFilters extends React.Component {
 
   render(){
-    let text = this.props.selected+'/'+this.props.count+' ('+this.props.percentage+') '+this.props.type
+    let text = this.props.selected+'/'+this.props.count+' ('+this.props.percentage+') '+this.props.type + 's'
     //let percent = this.props.selected/this.props.count*100
     return (
       <div className={styles.dataset_controls}>
