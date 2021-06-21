d7613a9b3a6d3ddd3c809391146ed12ac57faecb
Richard Challis <richard.challis@sanger.ac.uk>
2021-06-21 14:18:55 +0100

    upgrade minor versions

--- src/client/views/components/RecordModal.jsx
+++ src/client/views/components/RecordModal.jsx
@@ -1,53 +1,71 @@
-import React from 'react';
-import {ModalContainer, FlexDialog} from 'react-modal-dialog-react16';
-import styles from './Layout.scss'
-import CategoryDistribution from './CategoryDistribution'
+// import { FlexDialog, ModalContainer } from "react-modal-dialog-react16";
+
+import CategoryDistribution from "./CategoryDistribution";
+import Modal from "@material-ui/core/Modal";
+import React from "react";
+import styles from "./Layout.scss";
 
 export class RecordModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isShowingModal: (this.props.currentRecord || this.props.currentRecord === 0)
-    }
+      isShowingModal:
+        this.props.currentRecord || this.props.currentRecord === 0,
+    };
   }
 
-  componentDidUpdate(nextProps){
-    if (this.props.currentRecord !== nextProps.currentRecord){
+  componentDidUpdate(nextProps) {
+    if (this.props.currentRecord !== nextProps.currentRecord) {
       this.setState({
-        isShowingModal: (this.props.currentRecord || this.props.currentRecord === 0)
-      })
+        isShowingModal:
+          this.props.currentRecord || this.props.currentRecord === 0,
+      });
     }
   }
 
-  handleClick(){
-    this.setState({isShowingModal: true})
+  handleClick() {
+    this.setState({ isShowingModal: true });
   }
 
-  handleClose(){
-    this.props.dismiss()
-    this.setState({isShowingModal: false})
+  handleClose() {
+    this.props.dismiss();
+    this.setState({ isShowingModal: false });
   }
 
   render() {
-    if (this.state.isShowingModal){
+    if (this.state.isShowingModal) {
       return (
         <div className={styles.modal}>
-          <ModalContainer onClose={()=>this.handleClose()}>
-            <FlexDialog onClose={()=>this.handleClose()}>
-              <div className={styles.modal}>
-                <CategoryDistribution/>
-              </div>
-            </FlexDialog>
-          </ModalContainer>
+          <Modal
+            open={this.state.isShowingModal}
+            onClose={() => this.handleClose()}
+          >
+            {/* <FlexDialog onClose={() => this.handleClose()}> */}
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                backgroundColor: "white",
+                border: "none",
+                boxShadow: "none",
+                padding: "10px",
+                overflow: "visible",
+                "&:focus": {
+                  outline: "none",
+                },
+              }}
+            >
+              <CategoryDistribution />
+            </div>
+            {/* </FlexDialog> */}
+          </Modal>
         </div>
-      )
+      );
     }
-    return (
-      <div className={styles.modal}/>
-    )
-
+    return <div className={styles.modal} />;
   }
 }
 
-
-export default RecordModal
+export default RecordModal;
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,57 @@
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const GitRevisionPlugin = require('git-revision-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const GitRevisionPlugin = require("git-revision-webpack-plugin");
 // const combineLoaders = require('webpack-combine-loaders');
-const main = require('./src/config/main');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const main = require("./src/config/main");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const devMode = process.env.NODE_ENV !== 'production';
+const devMode = process.env.NODE_ENV !== "production";
 
-const BUILD_DIR = path.resolve(__dirname, 'dist/public');
-const APP_DIR = path.resolve(__dirname, 'src/client/views');
+const BUILD_DIR = path.resolve(__dirname, "dist/public");
+const APP_DIR = path.resolve(__dirname, "src/client/views");
 
 const gitRevisionPlugin = new GitRevisionPlugin();
 
-const protocol = main.https ? 'https' : 'http'
-const API_PORT = main.https ? 'https' : 'http'
+const protocol = main.https ? "https" : "http";
+const API_PORT = main.https ? "https" : "http";
 
 const config = {
   entry: {
-    main: ['@babel/polyfill', APP_DIR + '/index.jsx'],
+    main: ["@babel/polyfill", APP_DIR + "/index.jsx"],
     // cumulative: APP_DIR + '/components/CumulativePlot.jsx',
     // blob: APP_DIR + '/components/MainPlot.jsx',
     // snail: APP_DIR + '/components/SnailPlot.jsx',
     // table: APP_DIR + '/components/TablePlot.jsx'
   },
   output: {
-      publicPath: main.mode == 'production' ? main.basename + '/' : '/',
-      path: BUILD_DIR + '/',
-      // filename: 'js/bundle.js'
-      filename: devMode ? 'js/bundle.js' : 'js/[name].[hash].js',
-      chunkFilename: 'js/[id].js',
+    publicPath: main.mode == "production" ? main.basename + "/" : "/",
+    path: BUILD_DIR + "/",
+    // filename: 'js/bundle.js'
+    filename: devMode ? "js/bundle.js" : "js/[name].[hash].js",
+    chunkFilename: "js/[id].js",
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       maxInitialRequests: Infinity,
       minSize: 0,
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
           name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `npm.${packageName.replace('@', '')}`;
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace("@", "")}`;
           },
         },
-      }
-    }
+      },
+    },
   },
   devServer: {
     hot: true,
@@ -58,44 +60,46 @@ const config = {
     disableHostCheck: main.disableHostCheck,
     contentBase: BUILD_DIR,
     compress: true,
+    // headers: {
+    //   "Cross-Origin-Embedder-Policy": "require-corp",
+    //   "Cross-Origin-Opener-Policy": "same-origin",
+    // },
     port: main.client_port,
     proxy: {
-      '/api/**': { target: main.apiUrl }
-    }
+      "/api/**": { target: main.apiUrl },
+    },
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   plugins: [
     new MiniCssExtractPlugin({
       // filename: 'css/styles.css',
-      filename: devMode ? 'css/styles.css' : 'css/[name].[hash].css',
-      chunkFilename: 'css/[id].css'
+      filename: devMode ? "css/styles.css" : "css/[name].[hash].css",
+      chunkFilename: "css/[id].css",
     }),
     new webpack.DefinePlugin({
       API_URL: JSON.stringify(main.apiUrl),
-    	VERSION: JSON.stringify(main.version),
-    	BASENAME: JSON.stringify(main.basename),
+      VERSION: JSON.stringify(main.version),
+      BASENAME: JSON.stringify(main.basename),
       STATIC_THRESHOLD: JSON.stringify(main.staticThreshold),
       NOHIT_THRESHOLD: JSON.stringify(main.nohitThreshold),
       CIRCLE_LIMIT: JSON.stringify(main.circleLimit),
-    	HOME: JSON.stringify(protocol+'://'+main.hostname),
+      HOME: JSON.stringify(protocol + "://" + main.hostname),
       GIT_VERSION: JSON.stringify(gitRevisionPlugin.version()),
       COMMIT_HASH: JSON.stringify(gitRevisionPlugin.commithash()),
       BRANCH: JSON.stringify(gitRevisionPlugin.branch()),
       GA_ID: JSON.stringify(main.ga_id),
       GDPR_URL: JSON.stringify(main.gdpr_url),
-      DATASET_TABLE: (main.dataset_table ? true : false),
-      TARGET_TREE: (main.target_tree ? true : false),
-      MESSAGE: JSON.stringify(main.message)
+      DATASET_TABLE: main.dataset_table ? true : false,
+      TARGET_TREE: main.target_tree ? true : false,
+      MESSAGE: JSON.stringify(main.message),
     }),
     new HtmlWebpackPlugin({
       hash: true,
-      title: 'BlobToolKit - Viewer',
-      template: './src/client/index.html',
+      title: "BlobToolKit - Viewer",
+      template: "./src/client/index.html",
       // filename: BUILD_DIR + '/index.html'
     }),
-    new CopyWebpackPlugin([
-      { from: './src/client/favicon' }
-    ])
+    new CopyWebpackPlugin([{ from: "./src/client/favicon" }]),
   ],
   module: {
     rules: [
@@ -104,60 +108,63 @@ const config = {
         include: APP_DIR,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
       {
         test: /\.html$/,
         include: APP_DIR,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
+            loader: "html-loader",
+          },
+        ],
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader',
-            options: { injectType: 'singletonStyleTag' }
+            loader: "style-loader",
+            options: { injectType: "singletonStyleTag" },
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              modules: false
-            }
-          }
+              modules: false,
+            },
+          },
         ],
         include: [
           /node_modules/,
-          path.resolve(__dirname, 'src/client/views/style/node_modules.css')
-        ]
+          path.resolve(__dirname, "src/client/views/style/node_modules.css"),
+        ],
       },
       {
         test: /\.(sa|sc|c)ss$/,
         exclude: [
           /node_modules/,
-          path.resolve(__dirname, '/src/client/views/components/style/node_modules.css')
+          path.resolve(
+            __dirname,
+            "/src/client/views/components/style/node_modules.css"
+          ),
         ],
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             query: {
               modules: {
-	              localIdentName: "[name]__[local]___[hash:base64:5]",
-	            },
-	            sourceMap: true,
-              importLoaders: 2
-            }
+                localIdentName: "[name]__[local]___[hash:base64:5]",
+              },
+              sourceMap: true,
+              importLoaders: 2,
+            },
           },
-          'postcss-loader',
-          'sass-loader',
+          "postcss-loader",
+          "sass-loader",
         ],
       },
       // {
@@ -184,21 +191,18 @@ const config = {
       // },
       {
         test: /\.svg$/,
-        use: [
-          'svg-sprite-loader',
-          'svgo-loader'
-        ]
+        use: ["svg-sprite-loader", "svgo-loader"],
       },
       {
         test: /\.(gif|png|jpe?g)$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: 'img/[hash].[ext]',
-          publicPath: main.mode == 'production' ? main.basename + '/' :'/'
-        }
-      }
-    ]
-  }
+          name: "img/[hash].[ext]",
+          publicPath: main.mode == "production" ? main.basename + "/" : "/",
+        },
+      },
+    ],
+  },
 };
 
 module.exports = config;
