94c96a519a294c3d0510cdb1d3c938a92c7816b3
Richard Challis <richard.challis@sanger.ac.uk>
2022-01-28 11:29:31 +0000

    support bundle hosting

--- src/client/views/components/NoPlot.jsx
+++ src/client/views/components/NoPlot.jsx
@@ -1,51 +1,46 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import styles from './Plot.scss'
-import { getSearchTerm } from '../reducers/location'
+import React from "react";
+import { connect } from "react-redux";
+import { getSearchTerm } from "../reducers/location";
+import styles from "./Plot.scss";
 
-const apiUrl = API_URL || '/api/v1'
+const apiUrl = API_URL || "/api/v1";
 
 class NotFound extends React.Component {
-  render(){
-    let datasetId = this.props.datasetId
-    let searchTerm = this.props.searchTerm
-    let message
-    if (this.props.reason == 'api'){
-      message = `Unable to connect to API at ${apiUrl}`
-    }
-    else if (datasetId){
-      message = `Dataset ${datasetId} could not be found`
-    }
-    else {
-      message = `An unknown error has occured`
+  render() {
+    let datasetId = this.props.datasetId;
+    let searchTerm = this.props.searchTerm;
+    let message;
+    if (this.props.reason == "api") {
+      message = `Unable to connect to API at ${apiUrl}`;
+    } else if (datasetId) {
+      message = `Dataset ${datasetId} could not be found`;
+    } else {
+      message = `An unknown error has occured`;
     }
     return (
       <div className={styles.outer}>
         <div className={styles.centerContent}>
           <p className={styles.warn}>{message}</p>
-
         </div>
       </div>
-    )
+    );
   }
 }
 
 class NoPlot extends React.Component {
   constructor(props) {
     super(props);
-    this.mapStateToProps = state => {
+    this.mapStateToProps = (state) => {
       return {
-        searchTerm: getSearchTerm(state)
-      }
-    }
+        searchTerm: getSearchTerm(state),
+      };
+    };
   }
 
-  render(){
-    const ConnectedNoPlot = connect(
-      this.mapStateToProps
-    )(NotFound)
-    return <ConnectedNoPlot {...this.props}/>
+  render() {
+    const ConnectedNoPlot = connect(this.mapStateToProps)(NotFound);
+    return <ConnectedNoPlot {...this.props} />;
   }
 }
 
-export default NoPlot
+export default NoPlot;
