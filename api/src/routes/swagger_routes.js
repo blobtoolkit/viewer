38a483337f155cb66e42fb60699fdc4a881eaab9
Richard Challis <richard.challis@sanger.ac.uk>
2022-01-28 09:50:31 +0000

    package api as an executable

---
+++ api/src/routes/swagger_routes.js
@@ -0,0 +1,14 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+const config_swagger = require("../config/swagger");
+
+const swaggerSpec = swaggerJSDoc(config_swagger.options);
+
+module.exports = function (app, db) {
+  // serve swagger
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+  app.get("/swagger.json", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+};
