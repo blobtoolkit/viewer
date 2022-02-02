4ef1e176e78895a474b72f295828b2a6d080540c
Richard Challis <richard.challis@sanger.ac.uk>
2022-02-02 09:19:04 +0000

    set viewer basename variable when packaging

--- package.sh
+++ package.sh
@@ -9,20 +9,20 @@ TEMPLATE="<% if (variables) { %> \
 <% } %> \
 "
 
-npm run build &&
+#npm run build &&
 
-rm -rf ./ui/src/public &&
+#rm -rf ./ui/src/public &&
 
-tar -C ./dist -czf ./dist/blobtoolkit-viewer.tgz public &&
+#tar -C ./dist -czf ./dist/blobtoolkit-viewer.tgz public &&
 
-mv ./dist/public ./ui/src/ &&
+#mv ./dist/public ./ui/src/ &&
 
-mkdir -p ./ui/src/views &&
+#mkdir -p ./ui/src/views &&
 
-rm -rf ./ui/src/views/* &&
+#rm -rf ./ui/src/views/* &&
 
-sed 's:<!---->:'"$TEMPLATE"':' ./ui/src/public/index.html > ./ui/src/views/index.ejs &&
+#sed 's:<!---->:'"$TEMPLATE"':' ./ui/src/public/index.html > ./ui/src/views/index.ejs &&
 
-cd ./ui && pkg --compress GZip package.json && cd - &&
+BTK_BASENAME=/view pkg --compress GZip ui/package.json &&
 
-cd ./api && pkg --compress GZip package.json
+pkg --compress GZip api/package.json
