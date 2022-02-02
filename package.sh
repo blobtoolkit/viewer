72c691389d4c583e75b4f364221a67f2c2fede02
Richard Challis <richard.challis@sanger.ac.uk>
2022-02-02 09:26:04 +0000

    run full package.sh script

--- package.sh
+++ package.sh
@@ -9,19 +9,19 @@ TEMPLATE="<% if (variables) { %> \
 <% } %> \
 "
 
-#npm run build &&
+npm run build &&
 
-#rm -rf ./ui/src/public &&
+rm -rf ./ui/src/public &&
 
-#tar -C ./dist -czf ./dist/blobtoolkit-viewer.tgz public &&
+tar -C ./dist -czf ./dist/blobtoolkit-viewer.tgz public &&
 
-#mv ./dist/public ./ui/src/ &&
+mv ./dist/public ./ui/src/ &&
 
-#mkdir -p ./ui/src/views &&
+mkdir -p ./ui/src/views &&
 
-#rm -rf ./ui/src/views/* &&
+rm -rf ./ui/src/views/* &&
 
-#sed 's:<!---->:'"$TEMPLATE"':' ./ui/src/public/index.html > ./ui/src/views/index.ejs &&
+sed 's:<!---->:'"$TEMPLATE"':' ./ui/src/public/index.html > ./ui/src/views/index.ejs &&
 
 BTK_BASENAME=/view pkg --compress GZip ui/package.json &&
 
